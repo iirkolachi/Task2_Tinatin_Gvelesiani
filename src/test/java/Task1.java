b0e6a6b79a8df0a9af45cf9e5e178bb6e16b75f3
Tinatin Gvelesiani <tinatingvelesiani@credo.ge>
2024-10-08 00:21:06 +0400

    Task1_Gvelesiani

---
+++ src/test/java/Task1.java
@@ -0,0 +1,20 @@
+public class Task1 {
+    public static void main (String[] args) {
+        name();
+        age();
+        programmingLanguage();
+    }
+    public static void name() {
+        String name = "Tinatin";
+        System.out.println(name);
+    }
+    public static void age() {
+        int age = 25;
+        System.out.println(age);
+    }
+    public static void programmingLanguage() {
+        String programmingLanguage = "Java";
+        System.out.println(programmingLanguage);
+    }
+
+}
