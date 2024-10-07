f0ec078d3d97d74cb180a58f55f28dfa96a20b8c
Tinatin Gvelesiani <tinatingvelesiani@credo.ge>
2024-10-08 01:05:31 +0400

    Task4_Gvelesiani

---
+++ src/test/java/Task4.java
@@ -0,0 +1,8 @@
+public class Task4 {
+    public static void main (String[] args) {
+        String name = "Tinatin";
+        System.out.println("Hello, my name is " + name);
+        System.out.println(name.length());
+        System.out.println(name.toUpperCase());
+    }
+}
