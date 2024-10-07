3a83530ed749b74885a0300772f1a1f8e7abef97
Tinatin Gvelesiani <tinatingvelesiani@credo.ge>
2024-10-08 00:52:43 +0400

    Task3_Gvelesiani

---
+++ src/test/java/Task3.java
@@ -0,0 +1,21 @@
+public class Task3 {
+    public static void main (String[] args) {
+        int num1 = 5;
+        int num2 = 12;
+
+        int jami = num1 + num2;
+        System.out.println(jami);
+
+        int sxvaoba = num1 - num2;
+        System.out.println(sxvaoba);
+
+        int namravli = num1 * num2;
+        System.out.println(namravli);
+
+        int ganayofi = num1 / num2;
+        System.out.println(ganayofi);
+
+        int nashti = num1 % num2;
+        System.out.println(nashti);
+    }
+}
