85c2acc8ea4c84d150f3282db5bc10073fbe7c15
Tinatin Gvelesiani <tinatingvelesiani@credo.ge>
2024-10-08 00:44:15 +0400

    Task2_Gvelesiani

---
+++ src/test/java/Task2.java
@@ -0,0 +1,13 @@
+public class Task2 {
+    public static void main (String[] args) {
+        int age = 25;
+        double height = 1.67;
+        char firstLetter = 'T';
+        boolean isStudent = false;
+
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(firstLetter);
+        System.out.println(isStudent);
+    }
+}
