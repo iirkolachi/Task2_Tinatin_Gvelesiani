d152772679f6f4b6b1bf48f2372d0aedb71e3d51
Tinatin Gvelesiani <tinatingvelesiani@credo.ge>
2024-10-08 13:19:25 +0400

    Task6_Gvelesiani

---
+++ src/test/java/Task6.java
@@ -0,0 +1,7 @@
+public class Task6 {
+    public static void main (String[] args) {
+        double radiusi = 3.7;
+        double fartobi = Math.PI * Math.pow(radiusi,2); //რადგან ^ ეს ნიშანი ხარისხში ასაყვანად double-თან არ გამოიყენება, გამოვიყენე Math.pow
+        System.out.println(fartobi);
+    }
+}
